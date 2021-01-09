d7390e6a460cacd6bcb4a84e6b1136d7eaf2d870
Cam Duong <cam.duong@caseware.com>
2021-01-09 10:37:52 -0500

    Update firebase.js
    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAgOSLq79tj1vFRwKwRuCb7j9xZrwbJTFI",
-  authDomain: "fancy-todo-app-demo.firebaseapp.com",
-  projectId: "fancy-todo-app-demo",
-  storageBucket: "fancy-todo-app-demo.appspot.com",
-  messagingSenderId: "1028895211903",
-  appId: "1:1028895211903:web:fed8728406240438f65a4a",
-  measurementId: "G-81H85V337V"
+  // Add your firebase config here
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
